8cc72816f7a6d1a4458c550fddd4c0bf158b879f
Zhim <zhim@gmail.com>
2019-01-25 17:03:09 +0800

    add users routes

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,24 @@ router.get('/', async(req,res,next) =>{
     res.json(user)
 })
 
+router.post('/', async(req,res,next) =>{
+    const newUser = new User(req.body)
+    const user = await newUser.save()
+    res.json(user)
+})
+
+router.get('/:id', async(req,res,next) =>{
+    const {id} = req.params
+    const user = await User.findById(id)
+    res.json(user)
+})
+
+router.put('/:id', async(req,res,next) =>{
+    const {id} = req.params
+    const newUser = req.body
+    const result = await User.findByIdAndUpdate(id, newUser)
+    res.json({user: "Updated"})
+})
+
+
 module.exports = router
